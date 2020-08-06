9d45cfcf9548a5bfa06a54961e57ef41368d8e45
rachelle_c <rach8elle@gmail.com>
2020-08-06 12:34:45 -0600

    feat:firstpart of cards javascript

--- scripts/index.js
+++ scripts/index.js
@@ -30,3 +30,52 @@ form.addEventListener('submit', function (e) {
   toggleModal();
 }
 );
+
+
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg"
+  }
+];
+
+initialCards.forEach(data => {
+  const cardTemplate = document.querySelector('.card-template').content.querySelector('.elements__item');
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector('.elements__image');
+  const cardTitle = cardElement.querySelector('.elements__title');
+  const cardLikeButton = cardElement.querySelector('.elements__like-button');
+  const cardDeleteButton = cardElement.querySelector('.elements__delete-button');
+
+  cardTitle.textContent = data.name;
+  cardImage.style.backgroundImage = `url(${data.link})`;
+
+  const list = document.querySelector('.elements__items');
+
+  list.prepend(cardElement);
+
+
+});
+
+
+
