9064b91360beb265991a9d52e35f615793a66aa3
rachelle_c <rach8elle@gmail.com>
2020-08-07 05:01:50 -0600

    feat:begin new modal

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,46 @@
-const editModalButton = document.querySelector('.profile__edit-button');
-const closeModalButton = document.querySelector('.modal__close-button');
+//wrappers
 const modal = document.querySelector('.modal');
 const form = document.querySelector('.form');
-const nameInput = document.querySelector('.form__input_type_name');
-const aboutInput = document.querySelector('.form__input_type_about');
+
+//Buttons and other DOM elements
+const editModalButton = document.querySelector('.profile__edit-button');
+const closeModalButton = document.querySelector('.modal__close-button');
+const addCardModalButton = document.querySelector('profile__add-button');
+const addCardModalWindow = document.querySelector('.modal_type_add-card');
+
+//Profile section
 const profileName = document.querySelector('.profile__name');
 const profileAbout = document.querySelector('.profile__about');
 
+//Form Inputs
+const nameInput = document.querySelector('.form__input_type_name');
+const aboutInput = document.querySelector('.form__input_type_about');
+
 
 function toggleModal() {
-  if (modal.classList.contains('modal_opened') === false) {
+  modal.classList.toggle('modal_opened');
+  if (!modal.classList.contains('modal_opened')) {
     nameInput.value = profileName.textContent;
     aboutInput.value = profileAbout.textContent;
   }
-  modal.classList.toggle('modal_opened');
+
 }
 
 editModalButton.addEventListener('click', toggleModal);
 closeModalButton.addEventListener('click', toggleModal);
 
-
-
 form.addEventListener('submit', function (e) {
   e.preventDefault();
-
   profileName.textContent = nameInput.value;
   profileAbout.textContent = aboutInput.value;
-
   toggleModal();
 }
 );
+
+
+// addCardModalButton.addEventListener('click', () => {
+//   addCardModalWindow.classList.add('modal_opened');
+// });
 
 
 const initialCards = [
